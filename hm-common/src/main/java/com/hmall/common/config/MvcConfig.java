a0c19b5ef1264992440b4209f396395ec2458fc0
BeAWitch <109846146+BeAWitch@users.noreply.github.com>
2025-04-22 08:57:27 +0800

    网管登录校验和配置管理

---
+++ hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.hmall.common.config;
+
+import com.hmall.common.interceptors.UserInfoInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnClass(DispatcherServlet.class) // 只在微服务模块中生效，不在网关服务生效
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserInfoInterceptor());
+    }
+}
